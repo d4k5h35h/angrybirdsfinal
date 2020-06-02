c920ce5f68a3f319d9d440f374c21ad3a09ee0e6
DAKSHESH CHITTOORI <64137511+d4k5h35h@users.noreply.github.com>
2020-06-02 17:06:03 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,35 @@
+class Bird {
+  constructor(x,y)
+   { var options = {
+    'restitution':0.8,
+    'friction':1.0,
+    'density':1.0
+}
+this.body = Bodies.rectangle(x, y, 50,50, options);
+this.width = 50;
+this.height = 50;
+this.image = loadImage("sprites/bird.png");
+World.add(world, this.body);
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory =[];
+  }
+
+  display() {
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, 50, 50);
+    pop();
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+   
+
+    for(var i=0; i<this.trajectory.length; i++){
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+    }
+  }
+}
